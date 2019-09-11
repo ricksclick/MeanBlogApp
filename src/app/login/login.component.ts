daf76052b3d1834bd303654d2404f99d98580294
Prithvi Mohanty <Prithvi_Mohanty@epam.com>
2019-09-11 18:53:04 +0530

    created login component form, login service, user model

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { User } from '../models/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public user: User;
+  private subscriptions: Subscription = new Subscription();
+  constructor(private loginSvc: LoginService) {
+    this.user = new User();
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  validateLogin() {
+    if (this.user.username && this.user.password) {
+      this.subscriptions.add(
+        this.loginSvc.validateLogin(this.user).subscribe(result => {
+          console.log('login response from server', result);
+        }, console.warn)
+      );
+    } else {
+      alert('enter user name and password');
+    }
   }
 
 }
