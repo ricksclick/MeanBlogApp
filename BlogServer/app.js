1b423ff3f62ddf4b50f9e11c91e29ff406914513
Prithvi Mohanty <Prithvi_Mohanty@epam.com>
2019-09-11 19:16:37 +0530

    added proxy.json to client and nodemon pkg to server

--- BlogServer/app.js
+++ BlogServer/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
- 
-app.get('/api/user/login', (req, res) => {
-    res.send('Hello World!')
-});
- 
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/blogDb';
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/user/login', (req, res) => {
+    mongoose.connect(url, function (err) {
+        if (err) throw err;
+        console.log('connected successfully, username is ', req.body.username, ' password is ', req.body.password);
+    });
+})
+
 app.listen(3000, () => console.log('blog server running on port 3000!'));
