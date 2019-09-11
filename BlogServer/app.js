7505d8c45b6d578d26acd8984cec086b964e35bf
Prithvi Mohanty <Prithvi_Mohanty@epam.com>
2019-09-11 18:59:29 +0530

    Adding BlogServer Scaffolding

---
+++ BlogServer/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/api/user/login', (req, res) => {
+    res.send('Hello World!')
+});
+ 
+app.listen(3000, () => console.log('blog server running on port 3000!'));
