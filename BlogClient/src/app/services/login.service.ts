bcc323b182ca9702c1baa3530dff14b3691cea7b
Prithvi Mohanty <Prithvi_Mohanty@epam.com>
2019-09-12 17:13:06 +0530

    configured mongodb instead of mongoose, added login response to BlogServer

--- BlogClient/src/app/services/login.service.ts
+++ BlogClient/src/app/services/login.service.ts
@@ -1,18 +1,46 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user';
+
+import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http: HttpClient) { }
+  private headers = new HttpHeaders();
+  constructor(private http: HttpClient) {
+    this.headers.append('Content-Type', 'application/json');
+  }
 
   validateLogin(user: User) {
     return this.http.post('/api/user/login', {
       username: user.username,
       password: user.password
-    });
+    }, {
+      headers: this.headers
+    }).pipe(
+      map(this.extractData),
+      catchError(this.handleError)
+    );
+  }
+
+  private extractData(response: Response) {
+    return response.json();
+  }
+
+  private handleError(error: Response | any) {
+    let resMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      resMsg = body['message'];
+      console.log(body);
+      console.log(resMsg);
+    } else {
+      resMsg = error.message ? error.message : error.toString();
+    }
+    return Observable.throw(resMsg);
   }
 }
--- BlogServer/app.js
+++ BlogServer/app.js
@@ -1,36 +1,50 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const url = 'mongodb+srv://clustertest-4qkxq.mongodb.net/test';
 
-const User = require('./models/user');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+
+const CONNECTION_URL = "mongodb+srv://prithvi92:Ric123@clustertest-4qkxq.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "test";
+var database, collection;
+
+const app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/api/user/login', (req, res) => {
-    mongoose.connect(url, { useNewUrlParser: true }).then(() => {
 
-        User.find({
-            username: req.body.username, password: req.body.password
-        }, function (err, user) {
-            if (err) throw err;
-            if (user.length === 1) {
-                return res.status(200).json({
-                    status: 'success',
-                    data: user
-                })
-            } else {
-                return res.status(200).json({
-                    status: 'fail',
-                    message: 'Login Failed'
-                })
-            }
-
-        })
-    }).catch(err => { throw err; });
+app.listen(3000, () => {
+    console.log("Server running on localhost:3000");
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+        if (error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("user");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
 });
 
-app.listen(3000, () => console.log('blog server running on port 3000!'));
+app.post('/api/user/login', (req, res) => {
+    console.log(req.body);
+    collection.find(req.body).toArray(function (err, docs) {
+        if (err) {
+            res.sendStatus(404);
+            throw err;
+        } else {
+            console.log(docs);
+            if (docs.indexOf(req.body) > -1) {
+
+                res.send({
+                    status: 200,
+                    message: "login successful"
+                });
+            } else {
+                res.sendStatus(404);
+            }
+        }
+    });
+
+});
